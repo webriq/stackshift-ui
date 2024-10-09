bd09ee50aefd5e6bed50e8c2b1698bf4691cb6b1
WebriQ <info@webriq.com>
2024-10-09 17:58:53 +0800

    Initial commit

---
+++ packages/shared/src/client/header/theme-switch.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useMode } from "nextjs-darkmode/hooks";
+import { Switch } from "nextjs-darkmode/switch";
+import styles from "./header.module.scss";
+
+/** This is a wrapper around `nextjs-darkmode's ColorSwitch component to improve mobile view. */
+export default function ThemeSwitch(): JSX.Element {
+  const { mode } = useMode();
+  return (
+    <Switch className={styles.themeswitch}>
+      <span className="mb" suppressHydrationWarning>
+        {mode}
+      </span>
+    </Switch>
+  );
+}
