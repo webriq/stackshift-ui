d0202071e65385ce15698d568ef3265f1d4bc24c
MarielF WebriQ <mariel.filosopo@webriq.services>
2024-10-11 17:29:02 +0800

    fix package dependency for badge component

--- packages/components/common/badge/src/badge.tsx
+++ packages/components/common/badge/src/badge.tsx
@@ -1,6 +1,6 @@
 import { DefaultComponent, useStackShiftUIComponents } from "@stackshift-ui/system";
 import type { ElementType, HTMLProps, ReactNode } from "react";
-import { Flex } from "@stackshift-ui/react";
+import { Flex } from "@stackshift-ui/flex";
 import cn from "classnames";
 
 export interface BadgeProps extends Omit<HTMLProps<HTMLElement>, "as"> {
