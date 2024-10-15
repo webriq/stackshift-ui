775fc90bade4fe038779d7fd267cf19b5b708ce1
Roseller <enriquezroseller23@gmail.com>
2024-10-15 14:58:38 +0800

    temprary fixes for the storybook error

--- packages/core/react/src/react.tsx
+++ packages/core/react/src/react.tsx
@@ -1,13 +1,13 @@
 // Common
 export { Avatar } from "@webriq-test/avatar";
 export { Badge } from "@webriq-test/badge";
-export { defaultBlockStyle, Blockstyle } from "@webriq-test/blockstyle";
+export { Blockstyle, defaultBlockStyle } from "@webriq-test/blockstyle";
 export { Button } from "@webriq-test/button";
 export { Card } from "@webriq-test/card";
 export { Checkbox } from "@webriq-test/checkbox";
 export { CheckboxGroup } from "@webriq-test/checkbox-group";
 export { Form } from "@webriq-test/form";
-export { FormField } from "@webriq-test/form-field";
+// export { FormField } from "@webriq-test/form-field";
 export { Heading } from "@webriq-test/heading";
 export { Image } from "@webriq-test/image";
 export { Input } from "@webriq-test/input";
@@ -16,12 +16,12 @@ export { Link } from "@webriq-test/link";
 export { Radio } from "@webriq-test/radio";
 export { RadioGroup } from "@webriq-test/radio-group";
 export { Select } from "@webriq-test/select";
-export { StatsCard } from "@webriq-test/stats-card";
 export { SocialIcons } from "@webriq-test/social-icons";
-export { Text } from "@webriq-test/text";
-export { Textarea } from "@webriq-test/textarea";
+export { StatsCard } from "@webriq-test/stats-card";
 export { SwiperButton } from "@webriq-test/swiper-button";
 export { SwiperPagination } from "@webriq-test/swiper-pagination";
+export { Text } from "@webriq-test/text";
+export { Textarea } from "@webriq-test/textarea";
 export { WebriQForm } from "@webriq-test/webriq-form";
 export { YoutubeVideo } from "@webriq-test/youtube-video";
 
@@ -35,15 +35,15 @@ export {
 // Components
 export { AppPromo, AppPromo_A, AppPromo_B, AppPromo_C } from "@webriq-test/app-promo";
 export { Blog, Blog_A, Blog_B, Blog_C, Blog_D } from "@webriq-test/blog";
-export {
-  CallToAction,
-  CallToAction_A,
-  CallToAction_B,
-  CallToAction_C,
-  CallToAction_D,
-  CallToAction_E,
-} from "@webriq-test/call-to-action";
-export { Contact, Contact_A, Contact_B } from "@webriq-test/contact";
+// export {
+//   CallToAction,
+//   CallToAction_A,
+//   CallToAction_B,
+//   CallToAction_C,
+//   CallToAction_D,
+//   CallToAction_E,
+// } from "@webriq-test/call-to-action";
+// export { Contact, Contact_A, Contact_B } from "@webriq-test/contact";
 export { Cookies, Cookies_A } from "@webriq-test/cookies";
 export { Faqs, Faqs_A, Faqs_B, Faqs_C } from "@webriq-test/faqs";
 export {
@@ -58,7 +58,7 @@ export {
   Features_H,
 } from "@webriq-test/features";
 export { Footer, Footer_A, Footer_B, Footer_C, Footer_D } from "@webriq-test/footer";
-export { Header, Header_A, Header_B, Header_C, Header_D, Header_E } from "@webriq-test/header";
+// export { Header, Header_A, Header_B, Header_C, Header_D, Header_E } from "@webriq-test/header";
 export {
   HowItWorks,
   HowItWorks_A,
@@ -90,7 +90,7 @@ export {
   Portfolio_C,
   Portfolio_D,
 } from "@webriq-test/portfolio";
-export { SigninSignup, SigninSignup_A, SigninSignup_B } from "@webriq-test/signin-signup";
+// export { SigninSignup, SigninSignup_A, SigninSignup_B } from "@webriq-test/signin-signup";
 export { Statistics, Statistics_A, Statistics_B } from "@webriq-test/statistics";
 export { Team, Team_A, Team_B, Team_C, Team_D } from "@webriq-test/team";
 export {
@@ -113,5 +113,3 @@ export { Flex } from "@webriq-test/flex";
 export { Grid } from "@webriq-test/grid";
 export { GridItem } from "@webriq-test/grid-item";
 export { Section } from "@webriq-test/section";
-
-// Helper
