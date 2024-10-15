89b433e9c123f936529d819c7f6c97be11f830db
MarielF WebriQ <mariel.filosopo@webriq.services>
2024-10-15 11:25:22 +0800

    resolve build error

--- packages/core/react/src/react.tsx
+++ packages/core/react/src/react.tsx
@@ -6,11 +6,8 @@ export { Button } from "@stackshift-ui/button";
 export { Card } from "@stackshift-ui/card";
 export { Checkbox } from "@stackshift-ui/checkbox";
 export { CheckboxGroup } from "@stackshift-ui/checkbox-group";
-
-// TODO: uncomment these exports once the @stackshift-ui/system dependency module not found error is resolved in storybook
-// export { Form } from "@stackshift-ui/form";
-// export { FormField } from "@stackshift-ui/form-field";
-
+export { Form } from "@stackshift-ui/form";
+export { FormField } from "@stackshift-ui/form-field";
 export { Heading } from "@stackshift-ui/heading";
 export { Image } from "@stackshift-ui/image";
 export { Input } from "@stackshift-ui/input";
@@ -36,95 +33,79 @@ export {
 } from "@stackshift-ui/system";
 
 // Components
-export { AppPromo } from "@stackshift-ui/app-promo";
-export { AppPromo_A } from "@stackshift-ui/app-promo";
-export { AppPromo_B } from "@stackshift-ui/app-promo";
-export { AppPromo_C } from "@stackshift-ui/app-promo";
-export { Blog } from "@stackshift-ui/blog";
-export { Blog_A } from "@stackshift-ui/blog";
-export { Blog_B } from "@stackshift-ui/blog";
-export { Blog_C } from "@stackshift-ui/blog";
-export { Blog_D } from "@stackshift-ui/blog";
-export { CallToAction } from "@stackshift-ui/call-to-action";
-export { CallToAction_A } from "@stackshift-ui/call-to-action";
-export { CallToAction_B } from "@stackshift-ui/call-to-action";
-export { CallToAction_C } from "@stackshift-ui/call-to-action";
-export { CallToAction_D } from "@stackshift-ui/call-to-action";
-export { CallToAction_E } from "@stackshift-ui/call-to-action";
-export { Contact } from "@stackshift-ui/contact";
-export { Contact_A } from "@stackshift-ui/contact";
-export { Contact_B } from "@stackshift-ui/contact";
-export { Cookies } from "@stackshift-ui/cookies";
-export { Cookies_A } from "@stackshift-ui/cookies";
-export { Faqs } from "@stackshift-ui/faqs";
-export { Faqs_A } from "@stackshift-ui/faqs";
-export { Faqs_B } from "@stackshift-ui/faqs";
-export { Faqs_C } from "@stackshift-ui/faqs";
-export { Features } from "@stackshift-ui/features";
-export { Features_A } from "@stackshift-ui/features";
-export { Features_B } from "@stackshift-ui/features";
-export { Features_C } from "@stackshift-ui/features";
-export { Features_D } from "@stackshift-ui/features";
-export { Features_E } from "@stackshift-ui/features";
-export { Features_F } from "@stackshift-ui/features";
-export { Features_G } from "@stackshift-ui/features";
-export { Features_H } from "@stackshift-ui/features";
-export { Footer } from "@stackshift-ui/footer";
-export { Footer_A } from "@stackshift-ui/footer";
-export { Footer_B } from "@stackshift-ui/footer";
-export { Footer_C } from "@stackshift-ui/footer";
-export { Footer_D } from "@stackshift-ui/footer";
-export { Header } from "@stackshift-ui/header";
-export { Header_A } from "@stackshift-ui/header";
-export { Header_B } from "@stackshift-ui/header";
-export { Header_C } from "@stackshift-ui/header";
-export { Header_D } from "@stackshift-ui/header";
-export { Header_E } from "@stackshift-ui/header";
-export { HowItWorks } from "@stackshift-ui/how-it-works";
-export { HowItWorks_A } from "@stackshift-ui/how-it-works";
-export { HowItWorks_B } from "@stackshift-ui/how-it-works";
-export { HowItWorks_C } from "@stackshift-ui/how-it-works";
-export { HowItWorks_D } from "@stackshift-ui/how-it-works";
-export { HowItWorks_E } from "@stackshift-ui/how-it-works";
-export { LogoCloud } from "@stackshift-ui/logo-cloud";
-export { LogoCloud_A } from "@stackshift-ui/logo-cloud";
-export { LogoCloud_B } from "@stackshift-ui/logo-cloud";
-export { LogoCloud_C } from "@stackshift-ui/logo-cloud";
-export { LogoCloud_D } from "@stackshift-ui/logo-cloud";
-export { Navigation } from "@stackshift-ui/navigation";
-export { Navigation_A } from "@stackshift-ui/navigation";
-export { Navigation_B } from "@stackshift-ui/navigation";
-export { Navigation_C } from "@stackshift-ui/navigation";
-export { Navigation_D } from "@stackshift-ui/navigation";
-export { Navigation_E } from "@stackshift-ui/navigation";
-export { Newsletter } from "@stackshift-ui/newsletter";
-export { Newsletter_A } from "@stackshift-ui/newsletter";
-export { Newsletter_B } from "@stackshift-ui/newsletter";
-export { Portfolio } from "@stackshift-ui/portfolio";
-export { Portfolio_A } from "@stackshift-ui/portfolio";
-export { Portfolio_B } from "@stackshift-ui/portfolio";
-export { Portfolio_C } from "@stackshift-ui/portfolio";
-export { Portfolio_D } from "@stackshift-ui/portfolio";
-export { SigninSignup } from "@stackshift-ui/signin-signup";
-export { SigninSignup_A } from "@stackshift-ui/signin-signup";
-export { SigninSignup_B } from "@stackshift-ui/signin-signup";
-export { Statistics } from "@stackshift-ui/statistics";
-export { Statistics_A } from "@stackshift-ui/statistics";
-export { Statistics_B } from "@stackshift-ui/statistics";
-export { Team } from "@stackshift-ui/team";
-export { Team_A } from "@stackshift-ui/team";
-export { Team_B } from "@stackshift-ui/team";
-export { Team_C } from "@stackshift-ui/team";
-export { Team_D } from "@stackshift-ui/team";
-export { Testimonial } from "@stackshift-ui/testimonial";
-export { Testimonial_A } from "@stackshift-ui/testimonial";
-export { Testimonial_B } from "@stackshift-ui/testimonial";
-export { Testimonial_C } from "@stackshift-ui/testimonial";
-export { Testimonial_D } from "@stackshift-ui/testimonial";
-export { TextComponent } from "@stackshift-ui/text-component";
-export { TextComponent_A } from "@stackshift-ui/text-component";
-export { TextComponent_B } from "@stackshift-ui/text-component";
-export { TextComponent_C } from "@stackshift-ui/text-component";
+export { AppPromo, AppPromo_A, AppPromo_B, AppPromo_C } from "@stackshift-ui/app-promo";
+export { Blog, Blog_A, Blog_B, Blog_C, Blog_D } from "@stackshift-ui/blog";
+export {
+  CallToAction,
+  CallToAction_A,
+  CallToAction_B,
+  CallToAction_C,
+  CallToAction_D,
+  CallToAction_E,
+} from "@stackshift-ui/call-to-action";
+export { Contact, Contact_A, Contact_B } from "@stackshift-ui/contact";
+export { Cookies, Cookies_A } from "@stackshift-ui/cookies";
+export { Faqs, Faqs_A, Faqs_B, Faqs_C } from "@stackshift-ui/faqs";
+export {
+  Features,
+  Features_A,
+  Features_B,
+  Features_C,
+  Features_D,
+  Features_E,
+  Features_F,
+  Features_G,
+  Features_H,
+} from "@stackshift-ui/features";
+export { Footer, Footer_A, Footer_B, Footer_C, Footer_D } from "@stackshift-ui/footer";
+export { Header, Header_A, Header_B, Header_C, Header_D, Header_E } from "@stackshift-ui/header";
+export {
+  HowItWorks,
+  HowItWorks_A,
+  HowItWorks_B,
+  HowItWorks_C,
+  HowItWorks_D,
+  HowItWorks_E,
+} from "@stackshift-ui/how-it-works";
+export {
+  LogoCloud,
+  LogoCloud_A,
+  LogoCloud_B,
+  LogoCloud_C,
+  LogoCloud_D,
+} from "@stackshift-ui/logo-cloud";
+export {
+  Navigation,
+  Navigation_A,
+  Navigation_B,
+  Navigation_C,
+  Navigation_D,
+  Navigation_E,
+} from "@stackshift-ui/navigation";
+export { Newsletter, Newsletter_A, Newsletter_B } from "@stackshift-ui/newsletter";
+export {
+  Portfolio,
+  Portfolio_A,
+  Portfolio_B,
+  Portfolio_C,
+  Portfolio_D,
+} from "@stackshift-ui/portfolio";
+export { SigninSignup, SigninSignup_A, SigninSignup_B } from "@stackshift-ui/signin-signup";
+export { Statistics, Statistics_A, Statistics_B } from "@stackshift-ui/statistics";
+export { Team, Team_A, Team_B, Team_C, Team_D } from "@stackshift-ui/team";
+export {
+  Testimonial,
+  Testimonial_A,
+  Testimonial_B,
+  Testimonial_C,
+  Testimonial_D,
+} from "@stackshift-ui/testimonial";
+export {
+  TextComponent,
+  TextComponent_A,
+  TextComponent_B,
+  TextComponent_C,
+} from "@stackshift-ui/text-component";
 
 // Layout
 export { Container } from "@stackshift-ui/container";
