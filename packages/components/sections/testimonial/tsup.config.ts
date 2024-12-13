712986fe3d1b629d77fc265cb3b265dca7d6741f
MarielF WebriQ <info@webriq.com>
2024-12-13 11:13:43 +0800

    fixes for testimonial component

--- packages/components/sections/testimonial/tsup.config.ts
+++ packages/components/sections/testimonial/tsup.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, type Options } from "tsup";
+import { rdiPlugin } from "esbuild-plugin-rdi";
 import react18Plugin from "esbuild-plugin-react18";
 import cssPlugin from "esbuild-plugin-react18-css";
-import { rdiPlugin } from "esbuild-plugin-rdi";
+import { defineConfig, type Options } from "tsup";
 
 export default defineConfig(
   (options: Options) =>
@@ -18,6 +18,14 @@ export default defineConfig(
         cssPlugin({ generateScopedName: "[folder]__[local]" }),
         rdiPlugin(),
       ],
+      esbuildOptions(options: Options) {
+        options.define = {
+          ...options.define,
+          "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+        };
+        options.loader = { ".js": "jsx" };
+        return options;
+      },
       external: ["react"],
       ...options,
     }) as Options,
