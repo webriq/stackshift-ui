2ff36b7e8ef8378e2a41cd880910e5a81f965d4b
benj-webriq <info@webriq.com>
2025-07-01 16:39:27 +0800

    add: skeleton

---
+++ packages/components/common/skeleton/src/skeleton.test.tsx
@@ -0,0 +1,13 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { afterEach, describe, test } from "vitest";
+import { Skeleton } from "./skeleton";
+
+describe.concurrent("skeleton", () => {
+	afterEach(cleanup);
+
+	test("Dummy test - test if renders without errors", ({ expect }) => {
+		const clx = "my-class";
+		render(<Skeleton className={clx} />);
+		expect(screen.getByTestId("{ kebabCase name }}").classList).toContain(clx);
+	});
+});
---
+++ packages/components/common/skeleton/src/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@stackshift-ui/system";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
