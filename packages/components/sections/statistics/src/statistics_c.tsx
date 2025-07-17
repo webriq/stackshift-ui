9f2cf0535127304f7399670585efe866ea672485
benj-webriq <info@webriq.com>
2025-07-17 16:27:35 +0800

    chore: update Statistics section variants to use Card instead of StatsCard

--- packages/components/sections/statistics/src/statistics_c.tsx
+++ packages/components/sections/statistics/src/statistics_c.tsx
@@ -1,7 +1,8 @@
+import { Card, CardContent } from "@stackshift-ui/card";
 import { Container } from "@stackshift-ui/container";
 import { Flex } from "@stackshift-ui/flex";
 import { Section } from "@stackshift-ui/section";
-import { StatsCard } from "@stackshift-ui/stats-card";
+import { Text } from "@stackshift-ui/text";
 import { StatsProps } from ".";
 import { StatItems } from "./types";
 
@@ -19,17 +20,28 @@ function StatisticsItems({ stats }: { stats?: StatItems[] }) {
   if (!stats) return null;
 
   return (
-    <Flex wrap align="center" className="text-center ">
+    <Flex wrap align="center" className="text-center">
       {stats?.map((stat, index) => (
-        <StatsCard
-          key={index}
-          className="my-8 md:w-1/2 lg:w-1/4"
-          variant="stacked"
-          value={stat?.value ?? ""}
-          label={stat?.label ?? ""}
-          icon={`${stat?.mainImage?.image}`}
-          alt={stat?.mainImage?.alt}
-        />
+        <div key={index} className="my-8 md:w-1/2 lg:w-1/4 w-full px-4">
+          <Card className="bg-white">
+            <CardContent className="p-4 flex flex-col items-center">
+              {stat?.mainImage?.image && (
+                <div className="inline-block p-4 mb-3 rounded bg-secondary/50">
+                  <img
+                    src={stat?.mainImage?.image}
+                    width={24}
+                    height={24}
+                    alt={stat?.mainImage?.alt ?? "statistics-icon"}
+                  />
+                </div>
+              )}
+              <div>
+                <p className="text-2xl text-gray-500 font-bold">{stat?.value ?? ""}</p>
+                <Text muted>{stat?.label ?? ""}</Text>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       ))}
     </Flex>
   );
