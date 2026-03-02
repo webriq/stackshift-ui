039376d4537dfe2e26a89569979b6e9c1eb961c5
WebriQ <info@webriq.com>
2026-03-02 09:07:34 +0800

    fix: build error

--- apps/nextjs-pages/src/pages/_app.tsx
+++ apps/nextjs-pages/src/pages/_app.tsx
@@ -1,18 +1,13 @@
 import "@/styles/globals.css";
-import { StackShiftUIProvider, Toaster } from "@stackshift-ui/react";
+import { StackShiftUIProvider } from "@stackshift-ui/react";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 
 import { Image, Link } from "@/components/index";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const isToastPage = router.pathname === "/components/toast";
-
   return (
     <StackShiftUIProvider components={{ Link, Image }}>
       <Component {...pageProps} />
-      {/* {!isToastPage && <Toaster richColors closeButton />} */}
     </StackShiftUIProvider>
   );
 }
